b8c1405841e632c6fbdbb2b658f1e8c48ae03093
His royal prakness <prakhead@hotmail.com>
2018-10-16 22:19:04 +0000

    seconf update

--- load_monitor.sh
+++ load_monitor.sh
@@ -15,8 +15,8 @@ tmux send-keys "iftop" C-m
 # Setup a CoffeeScript compiler/watchdog pane
 tmux select-pane -t 0
 tmux split-window -v
-tmux send-keys "tailf /cache/steam/logs/access.log" C-m
+tmux send-keys "tail -f /cache/steam/logs/access.log" C-m
 tmux split-window -v
-tmux send-keys "tailf /cache/steam/logs/error.log" C-m
+tmux send-keys "tail -f /cache/steam/logs/error.log" C-m
 
 tmux select-layout tiled
