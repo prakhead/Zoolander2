35b6b6b46c46a84dc0ab45feb8290a2b10741a6b
Ian Fraser <prakhead@hotmail.com>
2018-10-19 09:09:18 +0000

    clean up for documentation

--- load_monitor.sh
+++ load_monitor.sh
@@ -5,18 +5,17 @@ tmux -2 new-session -d -s $SESSION
 
 # Setup a window for tailing log files
 tmux new-window -t $SESSION:1 -n 'Logs'
-tmux split-window -h
-tmux select-pane -t 0
-tmux send-keys "htop" C-m
-tmux select-pane -t 1
-tmux send-keys "docker stats" C-m
 tmux split-window -v
+tmux select-pane -t 1 
+tmux send-keys "htop" C-m
+tmux select-pane -t 2
 tmux send-keys "iftop" C-m
-# Setup a CoffeeScript compiler/watchdog pane
-tmux select-pane -t 0
+tmux split-window -v
+tmux send-keys "docker stats" C-m
+tmux select-pane -t 1
 tmux split-window -v
 tmux send-keys "tail -f /cache/steam/logs/access.log" C-m
 tmux split-window -v
 tmux send-keys "tail -f /cache/steam/logs/error.log" C-m
-
-tmux select-layout tiled
+tmux attach
+#tmux select-layout tiled
