971f742a5320de097aa738ca2af1e504a15cfb0d
ian <His royal Prakness>
2019-08-23 08:50:17 +0100

    Improved load_monitor.sh to behave better if run more than once

--- load_monitor.sh
+++ load_monitor.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 SESSION=$USER
-
+TMUX_PANE_INDEX=0
 tmux -2 new-session -d -s $SESSION
 
-# Setup a window for tailing log files
-tmux new-window -t $SESSION:1 -n 'Logs'
-tmux split-window -h
+
+# Check if panes already exist
+tmux select-pane -t 4
+RESULT=$?
+if [ $RESULT -eq 1 ]; then 
+  tmux split-window -h
+  tmux split-window -v
+  tmux split-window -v
+  tmux split-window -v
+fi
+
+# Test if we can access pane 0
 tmux select-pane -t 0
+RESULT=$?
+if [ $RESULT -eq 1 ]; then
+   TMUX_PANE_INDEX=1
+fi   
+tmux select-pane -t $((0 + $TMUX_PANE_INDEX))
+tmux send-keys "q" C-m # We quit first just in case htop is already running
 tmux send-keys "htop" C-m
-tmux select-pane -t 1
-tmux send-keys "docker stats" C-m
-tmux split-window -v
+tmux select-pane -t $((1 + $TMUX_PANE_INDEX))
+tmux send-keys "docker stats --format \"table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}\t{{.BlockIO}}\"" C-m
+tmux select-pane -t $((2 + $TMUX_PANE_INDEX))
+tmux send-keys "q" C-m # We quit first just in case iftop is already running
 tmux send-keys "iftop" C-m
-# Setup a CoffeeScript compiler/watchdog pane
-tmux select-pane -t 0
-tmux split-window -v
+tmux select-pane -t $((3 + $TMUX_PANE_INDEX)) 
 tmux send-keys "tail -f /cache/steam/logs/access.log" C-m
-tmux split-window -v
+tmux select-pane -t $((4 + $TMUX_PANE_INDEX))
 tmux send-keys "tail -f /cache/steam/logs/error.log" C-m
 
 tmux select-layout tiled
